d2c7e29e03e9723c358af6069c03cb2d0bda35cd
Muhammed Mishab <mdmishab124@gmail.com>
2024-12-31 17:22:23 +0530

    update

---
+++ funoon_fiesta/dist/assets/Login-Co2Gc4Je.js
@@ -0,0 +1,1 @@
+import{j as s}from"./animations-C7zvLhC6.js";import"./vendor-D-wlk8X7.js";import{A as o}from"./index-lAmFerv5.js";const i=()=>s.jsx("section",{children:s.jsx(o,{})});export{i as default};
--- funoon_fiesta/dist/assets/Login-DysqgCDf.js
+++
@@ -1,1 +0,0 @@
-import{j as s,A as n}from"./index-DM-f00sk.js";const t=()=>s.jsx("section",{children:s.jsx(n,{})});export{t as default};
